ac6f00787b0730ae3b326132a6021d731b0d30c6
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-10-08 15:53:06 +0300

    build kivy app

--- app/TODO.md
+++ app/TODO.md
@@ -7,6 +7,12 @@
 5. Bring over h5 model ✅
 
 6. Import Kivy dependencies ✅
-7. Build Layout
-8. Build update function, to keep update image when we move our head
-9. Bring preprocessing function, form Jupter
+7. Build Layout ✅
+8. Build update function, to keep update image when we move our head, and render the webcam ✅
+9. Bring preprocessing function, form Jupter ✅
+
+10. Bring over verification function
+11. Update verification function to handle new paths and save current frame
+12. Update verification function to set verified text
+13. Link verification function to button
+14. Setup Logger
--- app/facerec.py
+++ app/facerec.py
@@ -28,7 +28,7 @@ class CamApp(App):
 
     def build(self):
         # Main layout Components
-        self.img1 = Image(size_hint=(1, .8))  # Real time web cam feed
+        self.web_cam = Image(size_hint=(1, .8))  # Real time web cam feed
         self.button = Button(
             text="Verify", size_hint=(1, .1))  # Click to verify
         self.verification = Label(
@@ -36,11 +36,49 @@ class CamApp(App):
 
         # Add items to layout
         layout = BoxLayout(orientation='vertical')
-        layout.add_widget(self.img1)
+        layout.add_widget(self.web_cam)
         layout.add_widget(self.button)
         layout.add_widget(self.verification)
 
+        # Setup video capture device
+        # get image form webcam
+        self.capture = cv2.VideoCapture(0)
+        # schedule_interval: will do this event every x seconds
+        Clock.schedule_interval(self.update, 1.0/33.0)
+
         return layout
+
+    # Run continuously to get webcam feed
+    # convert raw OpenCV image -> texture can be rendered
+    def update(self, *args):
+
+        # Read frame form OpenCV
+        ret, frame = self.capture.read()
+        frame = frame[120:120+250, 200:200+250, :]
+
+        # Flip horizontall and convert image to texture
+        # save cv2 image
+        buf = cv2.flip(frame, 0).tostring()
+        # Create image texture with this frame sizes and color format (bgr is the color format of opencv images)
+        img_texture = Texture.create(
+            size=(frame.shape[0], frame.shape[1]), colorfmt='bgr')
+        # Convert image  to Texture
+        img_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        # Assign the texture to the webcam
+        self.web_cam.texture = img_texture
+
+    def preprocess(file_path):
+        # Read in image, as a bytes-like object, from file path
+        byte_img = tf.io.read_file(file_path)
+        # load in the image
+        img = tf.io.decode_jpeg(byte_img)
+
+        # Preprocessing steps
+        # 1. resizing the image to be 100x100
+        img = tf.image.resize(img, (100, 100))
+        # 2. Scale image to be between 0 and 1
+        img /= 255.0
+        return img
 
 
 if __name__ == '__main__':
